b27bd80d155942e91b03d2926df366a2d64f3623
tumatihub <jgsoaresdias@gmail.com>
2020-08-07 18:11:37 -0300

    Simple score UI with text mesh pro

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,12 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 public class Score : ScriptableObject
 {
     public IntegerVariable TotalScore;
 
+    private UnityAction ScoreChangeEvent;
+
     public void AddScore(int score)
     {
         TotalScore.Value = Mathf.Max(TotalScore.Value+score,0);
+        ScoreChangeEvent();
     }
+
+    public void Subscribe(UnityAction action)
+    {
+        ScoreChangeEvent += action;
+    }
+
+    public void Unsubscribe(UnityAction action)
+    {
+        ScoreChangeEvent -= action;
+    }
+
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public IntegerVariable TotalScore;
+    [SerializeField] TMP_Text _scoreText;
+    [SerializeField] Score _score;
+
+    private void OnEnable()
+    {
+        _score.Subscribe(UpdateScoreUI);    
+    }
+
+    private void Start()
+    {
+        TotalScore.Value = 0;    
+    }
+
+    public void UpdateScoreUI()
+    {
+        _scoreText.text = TotalScore.Value.ToString();
+    }
+
+    private void OnDisable()
+    {
+        _score.Unsubscribe(UpdateScoreUI);
+    }
+}
